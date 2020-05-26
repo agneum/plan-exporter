bcfa5a0d7b4e1b05e69d043f16d1baa6f0d7ab14
Richard Yen <richyen3@gmail.com>
2020-05-25 23:49:29 -0700

    Refactor code to remove code-duplication This consolidates visualizer targets into the visualizer package This will eventually make way for the possibility of passing in `--target=custom`, along with `--post_url` and `--plan_key`, so users can post their plans to local installs of say, depesz (see https://gitlab.com/depesz/explain.depesz.com/-/blob/master/Vagrantfile for an example of how someone might deploy their own explain.depesz.com instance)

--- visualizer/visualizer.go
+++ visualizer/visualizer.go
@@ -3,24 +3,50 @@ package visualizer
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/agneum/plan-exporter/pgscanner"
-	"github.com/agneum/plan-exporter/visualizer/dalibo"
-	"github.com/agneum/plan-exporter/visualizer/depesz"
-	"github.com/agneum/plan-exporter/visualizer/tensor"
+	"github.com/agneum/plan-exporter/client"
 )
 
-// New creates a new query plan exporter by visualizer type.
+type Visualizer struct {
+  postURL string
+  planKey string
+}
+
+// Export posts plan to a visualizer and returns link to the visualization plan page.
+func (this *Visualizer) Export(plan string) (string, error) {
+	formVal := url.Values{this.planKey: []string{plan}}
+
+	explainURL, err := client.MakeRequest(this.postURL, formVal)
+	if err != nil {
+		return "", fmt.Errorf("failed to make a request: %w", err)
+	}
+
+	return explainURL, nil
+}
+
+// New creates a new query plan exporter
 func New(visualizer string) (pgscanner.PlanExporter, error) {
+
 	switch visualizer {
-	case dalibo.VisualizerType:
-		return dalibo.New(), nil
+	case "dalibo":
+    return &Visualizer{
+		        postURL: "https://explain.dalibo.com/new",
+            planKey: "plan",
+    }, nil
 
-	case depesz.VisualizerType:
-		return depesz.New(), nil
+	case "tensor":
+    return &Visualizer{
+		        postURL: "https://explain.tensor.ru/explain",
+            planKey: "explain",
+    }, nil
 
-	case tensor.VisualizerType:
-		return tensor.New(), nil
+	default: //depesz
+    return &Visualizer{
+		        postURL: "https://explain.depesz.com/",
+            planKey: "plan",
+    }, nil
 	}
 
 	return nil, fmt.Errorf("unknown visualizer given %q", visualizer)
