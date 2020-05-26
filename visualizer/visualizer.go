ff7bac3d1270997bd147b908f7474394165bbc60
Richard Yen <richyen3@gmail.com>
2020-05-26 00:10:33 -0700

    golint changes

--- visualizer/visualizer.go
+++ visualizer/visualizer.go
@@ -5,20 +5,21 @@ import (
 	"fmt"
 	"net/url"
 
-	"github.com/agneum/plan-exporter/pgscanner"
 	"github.com/agneum/plan-exporter/client"
+	"github.com/agneum/plan-exporter/pgscanner"
 )
 
+// Visualizer consists of a URL for HTTP POST call, along with HTML element name
 type Visualizer struct {
-  postURL string
-  planKey string
+	postURL string
+	planKey string
 }
 
 // Export posts plan to a visualizer and returns link to the visualization plan page.
-func (this *Visualizer) Export(plan string) (string, error) {
-	formVal := url.Values{this.planKey: []string{plan}}
+func (v *Visualizer) Export(plan string) (string, error) {
+	formVal := url.Values{v.planKey: []string{plan}}
 
-	explainURL, err := client.MakeRequest(this.postURL, formVal)
+	explainURL, err := client.MakeRequest(v.postURL, formVal)
 	if err != nil {
 		return "", fmt.Errorf("failed to make a request: %w", err)
 	}
@@ -28,25 +29,24 @@ func (this *Visualizer) Export(plan string) (string, error) {
 
 // New creates a new query plan exporter
 func New(visualizer string) (pgscanner.PlanExporter, error) {
-
 	switch visualizer {
 	case "dalibo":
-    return &Visualizer{
-		        postURL: "https://explain.dalibo.com/new",
-            planKey: "plan",
-    }, nil
+		return &Visualizer{
+			postURL: "https://explain.dalibo.com/new",
+			planKey: "plan",
+		}, nil
 
 	case "tensor":
-    return &Visualizer{
-		        postURL: "https://explain.tensor.ru/explain",
-            planKey: "explain",
-    }, nil
+		return &Visualizer{
+			postURL: "https://explain.tensor.ru/explain",
+			planKey: "explain",
+		}, nil
 
 	default: //depesz
-    return &Visualizer{
-		        postURL: "https://explain.depesz.com/",
-            planKey: "plan",
-    }, nil
+		return &Visualizer{
+			postURL: "https://explain.depesz.com/",
+			planKey: "plan",
+		}, nil
 	}
 
 	return nil, fmt.Errorf("unknown visualizer given %q", visualizer)
